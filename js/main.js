950bda23c1935b5bd361badd3fd4048832c8e625
medanjp <kenleyindo@gmail.com>
2023-10-29 16:13:44 +0900

    HTML課題を提出します

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// jsを記述する際はここに記載していく
+
+$(function() {
+	setTimeout(function(){
+		$('.logo_fadein img').fadeIn(1000);
+	},500);
+	setTimeout(function(){
+		$('.logo_fadein').fadeOut(1000);
+	},2500);
+});
